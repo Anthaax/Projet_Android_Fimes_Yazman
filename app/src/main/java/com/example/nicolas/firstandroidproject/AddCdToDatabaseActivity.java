72ef8e48cb1d032f51de749de21f37c7699f1d96
Anthaax <guillaume.fimes@laposte.net>
2018-01-29 10:21:34 +0100

    DataReadAndWrite

--- app/src/main/java/com/example/nicolas/firstandroidproject/AddCdToDatabaseActivity.java
+++ app/src/main/java/com/example/nicolas/firstandroidproject/AddCdToDatabaseActivity.java
@@ -1,19 +1,30 @@
 package com.example.nicolas.firstandroidproject;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.lang.reflect.Array;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -25,7 +36,7 @@ import java.util.ListIterator;
  */
 
 public class AddCdToDatabaseActivity extends AppCompatActivity {
-
+    private String _fileName = "findYourCd.txt";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,57 +47,63 @@ public class AddCdToDatabaseActivity extends AppCompatActivity {
 
         CdParcelable cd = intent.getParcelableExtra("cd");
     }
-    private AlbumInfo[] readResult(String jsonStr) {
-        AlbumInfo[] albumInfos = null;
-        try {
-            JSONArray jsonArray = new JSONArray(jsonStr);
-            albumInfos = new AlbumInfo[jsonArray.length()];
-            for (int i = 0; i < jsonArray.length(); i++) {
-                AlbumInfo albumInfo = new AlbumInfo();
-                JSONObject jsonobject = jsonArray.getJSONObject(i);
-                albumInfo.id = jsonobject.getInt("id");
-                albumInfo.resource_url = jsonobject.getString("resource_url");
-                albumInfo.catno = jsonobject.getString("catno");
-                albumInfo.uri = jsonobject.getString("uri");
-                albumInfo.country = jsonobject.getString("country");
-                albumInfo.thumb = jsonobject.getString("thumb");
-                albumInfo.year = jsonobject.getString("year");
-                albumInfo.type = jsonobject.getString("year");
-                albumInfo.style = JSONArrayToStringArray(jsonobject.getJSONArray("style"));
-                albumInfo.format = JSONArrayToStringArray(jsonobject.getJSONArray("format"));
-                albumInfo.label = JSONArrayToStringArray(jsonobject.getJSONArray("label"));
-                albumInfo.genre = JSONArrayToStringArray(jsonobject.getJSONArray("genre"));
-                albumInfo.barcode = JSONArrayToStringArray(jsonobject.getJSONArray("barcode"));
-                albumInfo.community = JSONArrayToCommunity(jsonobject.getJSONArray("community"));
-                albumInfos[i] = albumInfo;
-            }
-        } catch ( JSONException ex){
 
-        }
+    private AlbumInfo[] ReadResult(String jsonStr) {
+        AlbumInfo[] albumInfos = new AlbumInfo[1];
+        Gson gson = new Gson();
+        Type list = new TypeToken<AlbumInfo[]>(){}.getType();
+        albumInfos = gson.fromJson(jsonStr, list);
         return  albumInfos;
     }
-    private String[] JSONArrayToStringArray(JSONArray json){
-        String[] array = new String[json.length()];
-        try {
-            for (int i = 0; i < json.length(); i++){
-                array[i] = json.getString(i);
-            }
-        }catch (JSONException ex) {
-            return null;
-        }
-        return  array;
+
+    private String JSONCreation(AlbumInfo[] albumInfos){
+        Gson gson = new Gson();
+        return  gson.toJson(albumInfos);
     }
-    private Community JSONArrayToCommunity(JSONArray jsonArray){
-        Community community = new Community();
-        try {
-            for (int i = 0; i < jsonArray.length(); i++){
-                JSONObject jsonobject = jsonArray.getJSONObject(i);
-                community.want = jsonobject.getInt("want");
-                community.have = jsonobject.getInt("have");
+    private void WriteToDatabase(AlbumInfo albumInfo){
+        AlbumInfo[] albumInfosOld = ReadResult(ReadFromDatabase());
+        AlbumInfo[] albumInfosNew = new AlbumInfo[albumInfo != null ? albumInfosOld.length + 1 : 1];
+        if(albumInfo != null)
+            System.arraycopy(albumInfosOld,0, albumInfosNew, 0, albumInfosOld.length);
+        albumInfosNew[albumInfo != null ? albumInfosOld.length : 0] = albumInfo;
+
+        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
+            try {
+                CreateFileIfNotExist();
+                String json = JSONCreation(albumInfosNew);
+                OutputStreamWriter writer = new OutputStreamWriter(this.openFileOutput(_fileName, Context.MODE_PRIVATE));
+                writer.write(json);
+                writer.close();
+            }catch (Exception ex){
+
             }
-        }catch (JSONException ex) {
-            return null;
         }
-        return  community;
+    }
+    private String ReadFromDatabase(){
+        String result ="";
+
+        try {
+            CreateFileIfNotExist();
+            InputStream inputStream = this.openFileInput(_fileName);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String build = "";
+            StringBuilder builder = new StringBuilder();
+            while ( (build = bufferedReader.readLine()) != null){
+                builder.append(build);
+            }
+            inputStream.close();
+            result = builder.toString();
+
+        }catch (Exception ex){
+
+        }
+        return  result;
+    }
+    private void CreateFileIfNotExist() throws IOException {
+        File file = new File(_fileName);
+        if(!file.exists()){
+            file.createNewFile();
+        }
     }
 }
